634d96f7147c74f4d1a24e897b85cfa1e9344632
Crash2022 <crash555@mail.ru>
2022-08-30 19:58:54 +0500

    refactoring button

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -10,36 +10,19 @@ export const Rating = () => {
 
     let [rating, setRating] = useState(0);
 
-    const addStar1 = () => {
-        setRating(1);
+    const addStar = (star:number) => {
+        setRating(star)
     }
-    const addStar2 = () => {
-        setRating(2);
-    }
-    const addStar3 = () => {
-        setRating(3);
-    }
-    const addStar4 = () => {
-        setRating(4);
-    }
-    const addStar5 = () => {
-        setRating(5);
-    }
-
-    /*const addStar = (e: any) => {
-        console.log(e.currentTarget.innerText)
-    }*/
 
     return (
         <div>
             <h2 style={{marginBottom: "10px"}}>Rating</h2>
-            <Star selected={rating > 0}/><Button name={"1"} callBack={addStar1}/>
-            <Star selected={rating > 1}/><Button name={"2"} callBack={addStar2}/>
-            <Star selected={rating > 2}/><Button name={"3"} callBack={addStar3}/>
-            <Star selected={rating > 3}/><Button name={"4"} callBack={addStar4}/>
-            <Star selected={rating > 4}/><Button name={"5"} callBack={addStar5}/>
+            <Star selected={rating > 0}/><Button name={"1"} callBack={addStar}/>
+            <Star selected={rating > 1}/><Button name={"2"} callBack={addStar}/>
+            <Star selected={rating > 2}/><Button name={"3"} callBack={addStar}/>
+            <Star selected={rating > 3}/><Button name={"4"} callBack={addStar}/>
+            <Star selected={rating > 4}/><Button name={"5"} callBack={addStar}/>
             <span><button style={{marginLeft: "10px"}} onClick={()=>{setRating(0)}}>not rated</button></span>
-            {/*<span><button style={{marginLeft: "10px"}} onClick={addStar}>myname</button></span>*/}
         </div>
     );
 }
