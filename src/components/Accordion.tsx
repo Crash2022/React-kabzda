b9c8a2917b8d45db3808ae9a4e9e84224a835326
Crash2022 <crash555@mail.ru>
2022-09-06 22:08:50 +0500

    make controlled accordion2

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -11,17 +11,18 @@ export type AccordionPropsType = {
 
 export const Accordion: FC<AccordionPropsType> = ({title, menu1CollapsedControlled, setMenu1CollapsedControlled}) => {
 
-    let [menu2CollapsedUncontrolled, setMenu2CollapsedUncontrolled] = useState<boolean>(false);
+    /*let [menu2CollapsedUncontrolled, setMenu2CollapsedUncontrolled] = useState<boolean>(false);*/
 
     /*const buttonHandlerCollapsed = () => {
+
         setMenu2CollapsedUncontrolled(!menu2CollapsedUncontrolled);
     }*/
 
     return (
         <div style={{margin: "20px 0px"}}>
             <AccordionTitle title={title} collapseMenu={ ()=> setMenu1CollapsedControlled(!menu1CollapsedControlled) }/>
-            {/*<AccordionTitle title={title} collapseMenu={buttonHandlerCollapsed}/>*/}
             { !menu1CollapsedControlled ? <AccordionBody/> : '' }
+            {/*<AccordionTitle title={title} collapseMenu={buttonHandlerCollapsed}/>*/}
             {/*{ !menu2CollapsedUncontrolled ? <AccordionBody/> : '' }*/}
         </div>
     );
